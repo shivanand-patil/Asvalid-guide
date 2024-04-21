fea1b594187d56305e41bc8d8493a682dc480e05
Shivanand Patil <shivanandpatil963@gmail.com>
2024-04-21 06:35:25 +0000

    server restart added

--- try/test.sh
+++ try/test.sh
@@ -1,24 +1,61 @@
+# #!/bin/bash
+
+# python3 valid.py
+
+# if [ $? -ne 0 ]; then
+#     echo "Structure not good, server-valid.py encountered errors. Exiting."
+#     exit 1
+# fi
+
+# # proceed if no errors
+# file_path="/etc/aerospike/aerospike.conf"
+# echo " Structure is good, Evaluating $file_path against config schema "
+
+
+# # get AS version
+# version=$(asd --version | awk '{print $5}' | cut -d. -f1-3)
+
+
+# # asconfig
+# if asconfig validate --aerospike-version "$version" "$file_path"; then
+#     echo "Validation successful for $version"
+# else
+#     echo "Validation failed for $version"
+# fi
+
 #!/bin/bash
 
+# Validate configuration with server-valid.py
 python3 valid.py
 
+# Check if server-valid.py encountered errors
 if [ $? -ne 0 ]; then
     echo "Structure not good, server-valid.py encountered errors. Exiting."
     exit 1
 fi
 
-# proceed if no errors
+# Proceed if no errors
 file_path="/etc/aerospike/aerospike.conf"
-echo " Structure is good, Evaluating $file_path against config schema "
 
 
-# get AS version
+# Get Aerospike version
 version=$(asd --version | awk '{print $5}' | cut -d. -f1-3)
 
+echo "Structure is good. Evaluating $file_path against version $version config schema."
 
-# asconfig
+# Validate configuration using asconfig
 if asconfig validate --aerospike-version "$version" "$file_path"; then
-    echo "Validation successful"
+
+    echo "Validation successful for Aerospike version $version."
+
+    read -p "Do you want to restart the server to apply changes? (y/n): " choice
+    if [ "$choice" == "y" ]; then
+        systemctl restart aerospike
+        echo "Aerospike server restarted successfully."
+    else
+        echo "No action taken. Exiting"
+    fi
 else
-    echo "Validation failed"
+    echo "Validation failed for Aerospike version $version."
 fi
+

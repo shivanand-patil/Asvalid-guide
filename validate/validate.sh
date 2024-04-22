16a317a4e910e8afb73a921b4b288ce43bf42b08
Shivanand Patil <70444072+shivanand-patil@users.noreply.github.com>
2024-04-22 19:58:43 +0530

    ed

--- validate/validate.sh
+++ validate/validate.sh
@@ -1,41 +1,16 @@
-# #!/bin/bash
-
-# python3 valid.py
-
-# if [ $? -ne 0 ]; then
-#     echo "Structure not good, server-valid.py encountered errors. Exiting."
-#     exit 1
-# fi
-
-# # proceed if no errors
-# file_path="/etc/aerospike/aerospike.conf"
-# echo " Structure is good, Evaluating $file_path against config schema "
-
-
-# # get AS version
-# version=$(asd --version | awk '{print $5}' | cut -d. -f1-3)
-
-
-# # asconfig
-# if asconfig validate --aerospike-version "$version" "$file_path"; then
-#     echo "Validation successful for $version"
-# else
-#     echo "Validation failed for $version"
-# fi
-
 #!/bin/bash
 
-# Validate configuration with server-valid.py
+# Validate python script
 python3 validate.py
 
-# Check if server-valid.py encountered errors
+# Check for errors
 if [ $? -ne 0 ]; then
     echo "Structure not good, server-valid.py encountered errors. Exiting."
     exit 1
 fi
 
 # Proceed if no errors
-file_path="aerospike.conf"
+file_path="/etc/aerospike/aerospike.conf"
 
 
 # Get Aerospike version
